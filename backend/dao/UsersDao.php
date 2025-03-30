522b042844a607b1642900257b44f181ee847978
melisa3030 <Melisa.ahmetovic@stu.ibu.edu.ba>
2025-03-31 00:02:51 +0200

    add UsersDao class for user data access

---
+++ backend/dao/UsersDao.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once __DIR__ . '/BaseDao.php';
+
+class UsersDao extends BaseDao
+{
+  public function __construct()
+  {
+    parent::__construct("users");
+  }
+
+  public function getByEmail($email)
+  {
+    $stmt = $this->connection->prepare("SELECT * FROM users WHERE email = :email");
+    $stmt->bindParam(':email', $email);
+    $stmt->execute();
+    return $stmt->fetch();
+  }
+
+  public function getByUsername($username)
+  {
+    $stmt = $this->connection->prepare("SELECT * FROM users WHERE username = :username");
+    $stmt->bindParam(':username', $username);
+    $stmt->execute();
+    return $stmt->fetch();
+  }
+}
