2eb005de5f505f6c794cb6d1a944c81ab33802dc
melisa3030 <Melisa.ahmetovic@stu.ibu.edu.ba>
2025-03-31 13:09:16 +0200

    update validation in helpers to check for unexpected fields

--- backend/helpers.php
+++ backend/helpers.php
@@ -3,7 +3,12 @@ function validateBody($requiredFields, $data)
 {
   foreach ($requiredFields as $field) {
     if (!isset($data[$field]) || empty(trim($data[$field]))) {
-      Flight::jsonHalt((["message" => "Missing required field: $field"]), 400);
+      Flight::jsonHalt(["message" => "Missing required field: $field"], 400);
     }
+  }
+
+  $unexpectedFields = array_diff(array_keys($data), $requiredFields);
+  if (!empty($unexpectedFields)) {
+    Flight::jsonHalt(["message" => "Unexpected fields: " . implode(", ", $unexpectedFields)], 400);
   }
 }
