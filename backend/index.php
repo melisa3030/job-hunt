a103165c18cb3129ba0ed954bf0b4ace480e70f2
melisa3030 <Melisa.ahmetovic@stu.ibu.edu.ba>
2025-04-02 13:19:28 +0200

    add newly created routes

--- backend/index.php
+++ backend/index.php
@@ -1,7 +1,13 @@
 <?php
-require 'vendor/autoload.php';
+
+require_once __DIR__ . "/vendor/autoload.php";
+
 require_once __DIR__ . '/routes/users.php';
 require_once __DIR__ . '/routes/companies.php';
+require_once __DIR__ . '/routes/perks.php';
+require_once __DIR__ . '/routes/tags.php';
+require_once __DIR__ . '/routes/job_titles.php';
+require_once __DIR__ . '/routes/job_categories.php';
 
 Flight::route('GET /', function () {
   echo 'Welcome to the Job Hunt API!';
