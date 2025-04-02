8b61e044073ac353ac651e030f6f15e7f42bca05
melisa3030 <Melisa.ahmetovic@stu.ibu.edu.ba>
2025-04-02 13:19:12 +0200

    add JobTitlesDao and routes for managing job titles

---
+++ backend/dao/JobTitlesDao.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once __DIR__ . '/../dao/JobTitlesDao.php';
+
+class JobTitlesDao extends BaseDao
+{
+    public function __construct()
+    {
+        parent::__construct("job_titles");
+    }
+
+    public function getByName($name)
+    {
+        $query = "SELECT * FROM job_titles WHERE name = :name";
+        $stmt = $this->connection->prepare($query);
+        $stmt->bindParam(':name', $name);
+        $stmt->execute();
+        return $stmt->fetch();
+    }
+}
