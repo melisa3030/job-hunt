072a0a4bb7ac547a1ba80f2513a7145c2ec936d4
melisa3030 <Melisa.ahmetovic@stu.ibu.edu.ba>
2025-04-03 16:31:35 +0200

    update index.php file with additional FlighPHP routes

--- backend/index.php
+++ backend/index.php
@@ -8,6 +8,14 @@ require_once __DIR__ . '/routes/perks.php';
 require_once __DIR__ . '/routes/tags.php';
 require_once __DIR__ . '/routes/job_titles.php';
 require_once __DIR__ . '/routes/job_categories.php';
+require_once __DIR__ . '/routes/reviews.php';
+require_once __DIR__ . '/routes/review_tags.php';
+require_once __DIR__ . '/routes/jobs.php';
+require_once __DIR__ . '/routes/job_tags.php';
+require_once __DIR__ . '/routes/job_perks.php';
+require_once __DIR__ . '/routes/job_categories.php';
+require_once __DIR__ . '/routes/bookmarked_jobs.php';
+require_once __DIR__ . '/routes/applications.php';
 
 Flight::route('GET /', function () {
   echo 'Welcome to the Job Hunt API!';
