10d626251dac6a4353093324ebd30fec1a3b84d3
melisa3030 <Melisa.ahmetovic@stu.ibu.edu.ba>
2025-04-27 12:53:28 +0200

    add getCompanyByName method to CompaniesDao for fetching company details by name

--- backend/dao/CompaniesDao.php
+++ backend/dao/CompaniesDao.php
@@ -8,4 +8,12 @@ class CompaniesDao extends BaseDao
   {
     parent::__construct("companies");
   }
+
+  public function getCompanyByName($name)
+  {
+    $stmt = $this->connection->prepare("SELECT * FROM companies WHERE name = :name");
+    $stmt->bindParam(":name", $name);
+    $stmt->execute();
+    return $stmt->fetch();
+  }
 }
