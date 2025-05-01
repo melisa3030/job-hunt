b071e8f074750df0e532e33c77799092618e4a50
melisa3030 <Melisa.ahmetovic@stu.ibu.edu.ba>
2025-05-01 12:49:35 +0200

    register ReviewTagsService and PerksService in index.php

--- backend/index.php
+++ backend/index.php
@@ -21,11 +21,17 @@ require_once __DIR__ . '/services/UserService.php';
 require_once __DIR__ . '/services/TagsService.php';
 require_once __DIR__ . '/services/ReviewsService.php';
 require_once __DIR__ . '/services/CompaniesService.php';
+require_once __DIR__ . '/services/ReviewTagsService.php';
+require_once __DIR__ . '/services/PerksService.php';
 
 Flight::register('userService', 'UserService');
 Flight::register('tagsService', 'TagsService');
 Flight::register('reviewsService', 'ReviewsService');
 Flight::register('companiesService', 'CompaniesService');
+Flight::register('reviewTagsService', 'ReviewTagsService');
+Flight::register('perksService', 'PerksService');
+
+
 
 Flight::route('GET /', function () {
     echo 'Welcome to the Job Hunt API!';
