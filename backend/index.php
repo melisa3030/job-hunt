8293a017a95029ab67f73a4e96f7da5ca52cf68b
melisa3030 <Melisa.ahmetovic@stu.ibu.edu.ba>
2025-05-03 18:15:14 +0200

    add missing service registrations in index.php

--- backend/index.php
+++ backend/index.php
@@ -2,6 +2,7 @@
 
 require_once __DIR__ . "/vendor/autoload.php";
 
+// Routes
 require_once __DIR__ . '/routes/users.php';
 require_once __DIR__ . '/routes/companies.php';
 require_once __DIR__ . '/routes/perks.php';
@@ -17,12 +18,20 @@ require_once __DIR__ . '/routes/job_categories.php';
 require_once __DIR__ . '/routes/bookmarked_jobs.php';
 require_once __DIR__ . '/routes/applications.php';
 
+// Services
 require_once __DIR__ . '/services/UserService.php';
 require_once __DIR__ . '/services/TagsService.php';
 require_once __DIR__ . '/services/ReviewsService.php';
 require_once __DIR__ . '/services/CompaniesService.php';
 require_once __DIR__ . '/services/ReviewTagsService.php';
 require_once __DIR__ . '/services/PerksService.php';
+require_once __DIR__ . '/services/JobsService.php';
+require_once __DIR__ . '/services/JobTitlesService.php';
+require_once __DIR__ . '/services/JobCategoriesService.php';
+require_once __DIR__ . '/services/JobTagsService.php';
+require_once __DIR__ . '/services/JobPerksService.php';
+require_once __DIR__ . '/services/BookmarkedJobsService.php';
+require_once __DIR__ . '/services/ApplicationsService.php';
 
 Flight::register('userService', 'UserService');
 Flight::register('tagsService', 'TagsService');
@@ -30,6 +39,14 @@ Flight::register('reviewsService', 'ReviewsService');
 Flight::register('companiesService', 'CompaniesService');
 Flight::register('reviewTagsService', 'ReviewTagsService');
 Flight::register('perksService', 'PerksService');
+Flight::register('jobsService', 'JobsService');
+Flight::register('jobTitlesService', 'JobTitlesService');
+Flight::register('jobCategoriesService', 'JobCategoriesService');
+Flight::register('jobTagsService', 'JobTagsService');
+Flight::register('jobPerksService', 'JobPerksService');
+Flight::register('bookmarkedJobsService', 'BookmarkedJobsService');
+Flight::register('applicationsService', 'ApplicationsService');
+
 
 
 
