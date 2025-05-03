4d83470538a542bc5fcf7bd90d7a31553445e447
melisa3030 <Melisa.ahmetovic@stu.ibu.edu.ba>
2025-05-03 18:46:16 +0200

    update require paths to use 'rest' directory for routes and services

--- backend/index.php
+++ backend/index.php
@@ -3,35 +3,35 @@
 require_once __DIR__ . "/vendor/autoload.php";
 
 // Routes
-require_once __DIR__ . '/routes/users.php';
-require_once __DIR__ . '/routes/companies.php';
-require_once __DIR__ . '/routes/perks.php';
-require_once __DIR__ . '/routes/tags.php';
-require_once __DIR__ . '/routes/job_titles.php';
-require_once __DIR__ . '/routes/job_categories.php';
-require_once __DIR__ . '/routes/reviews.php';
-require_once __DIR__ . '/routes/review_tags.php';
-require_once __DIR__ . '/routes/jobs.php';
-require_once __DIR__ . '/routes/job_tags.php';
-require_once __DIR__ . '/routes/job_perks.php';
-require_once __DIR__ . '/routes/job_categories.php';
-require_once __DIR__ . '/routes/bookmarked_jobs.php';
-require_once __DIR__ . '/routes/applications.php';
+require_once __DIR__ . '/rest/routes/users.php';
+require_once __DIR__ . '/rest/routes/companies.php';
+require_once __DIR__ . '/rest/routes/perks.php';
+require_once __DIR__ . '/rest/routes/tags.php';
+require_once __DIR__ . '/rest/routes/job_titles.php';
+require_once __DIR__ . '/rest/routes/job_categories.php';
+require_once __DIR__ . '/rest/routes/reviews.php';
+require_once __DIR__ . '/rest/routes/review_tags.php';
+require_once __DIR__ . '/rest/routes/jobs.php';
+require_once __DIR__ . '/rest/routes/job_tags.php';
+require_once __DIR__ . '/rest/routes/job_perks.php';
+require_once __DIR__ . '/rest/routes/job_categories.php';
+require_once __DIR__ . '/rest/routes/bookmarked_jobs.php';
+require_once __DIR__ . '/rest/routes/applications.php';
 
 // Services
-require_once __DIR__ . '/services/UserService.php';
-require_once __DIR__ . '/services/TagsService.php';
-require_once __DIR__ . '/services/ReviewsService.php';
-require_once __DIR__ . '/services/CompaniesService.php';
-require_once __DIR__ . '/services/ReviewTagsService.php';
-require_once __DIR__ . '/services/PerksService.php';
-require_once __DIR__ . '/services/JobsService.php';
-require_once __DIR__ . '/services/JobTitlesService.php';
-require_once __DIR__ . '/services/JobCategoriesService.php';
-require_once __DIR__ . '/services/JobTagsService.php';
-require_once __DIR__ . '/services/JobPerksService.php';
-require_once __DIR__ . '/services/BookmarkedJobsService.php';
-require_once __DIR__ . '/services/ApplicationsService.php';
+require_once __DIR__ . '/rest/services/UserService.php';
+require_once __DIR__ . '/rest/services/TagsService.php';
+require_once __DIR__ . '/rest/services/ReviewsService.php';
+require_once __DIR__ . '/rest/services/CompaniesService.php';
+require_once __DIR__ . '/rest/services/ReviewTagsService.php';
+require_once __DIR__ . '/rest/services/PerksService.php';
+require_once __DIR__ . '/rest/services/JobsService.php';
+require_once __DIR__ . '/rest/services/JobTitlesService.php';
+require_once __DIR__ . '/rest/services/JobCategoriesService.php';
+require_once __DIR__ . '/rest/services/JobTagsService.php';
+require_once __DIR__ . '/rest/services/JobPerksService.php';
+require_once __DIR__ . '/rest/services/BookmarkedJobsService.php';
+require_once __DIR__ . '/rest/services/ApplicationsService.php';
 
 Flight::register('userService', 'UserService');
 Flight::register('tagsService', 'TagsService');
