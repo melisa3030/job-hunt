c4273ea07641a9cdcdb0e585cd6ef598a3a8016b
melisa3030 <Melisa.ahmetovic@stu.ibu.edu.ba>
2025-05-03 18:17:19 +0200

    refactor job_perks routes to utilize JobPerksService

---
+++ backend/services/JobPerksService.php
@@ -0,0 +1,71 @@
+<?php
+
+require_once __DIR__ . '/../dao/JobPerksDao.php';
+require_once __DIR__ . '/../dao/PerksDao.php';
+require_once __DIR__ . '/../dao/JobsDao.php';
+
+require_once __DIR__ . '/../helpers.php';
+
+class JobPerksService
+{
+  private $jobPerksDao;
+  private $perksDao;
+  private $jobsDao;
+
+  public function __construct()
+  {
+    $this->jobPerksDao = new JobPerksDao();
+    $this->perksDao = new PerksDao();
+    $this->jobsDao = new JobsDao();
+  }
+
+  public function getAllJobPerks()
+  {
+    return $this->jobPerksDao->getAll();
+  }
+
+  public function getJobPerksByJobId($job_id)
+  {
+    $jobPerks = $this->jobPerksDao->getByJobId($job_id);
+    if ($jobPerks) {
+      return $jobPerks;
+    } else {
+      throw new Exception("No job perks found for provided job", 404);
+    }
+  }
+
+  public function getJobPerksByPerkId($perk_id)
+  {
+    $jobPerks = $this->jobPerksDao->getByPerkId($perk_id);
+    if ($jobPerks) {
+      return $jobPerks;
+    } else {
+      throw new Exception("No job perks found for provided perk", 404);
+    }
+  }
+
+  public function createJobPerk($data)
+  {
+    validateBody(['job_id', 'perk_id'], $data);
+
+    $job_id = $data['job_id'];
+    $perk_id = $data['perk_id'];
+
+    $job = $this->jobsDao->getById($job_id);
+    $perk = $this->perksDao->getById($perk_id);
+
+    if (!$job) {
+      throw new Exception("Job not found", 404);
+    }
+
+    if (!$perk) {
+      throw new Exception("Perk not found", 404);
+    }
+
+    if ($this->jobPerksDao->insert($data)) {
+      return ["message" => "Job perk created successfully"];
+    } else {
+      throw new Exception("Error creating job perk", 500);
+    }
+  }
+}
