c6e845268b500ac932d0c044c8c554394ad87c02
melisa3030 <Melisa.ahmetovic@stu.ibu.edu.ba>
2025-05-17 14:33:36 +0200

    implement getByField method in ReviewsDao for retrieving reviews by specified field

--- backend/rest/dao/ReviewsDao.php
+++ backend/rest/dao/ReviewsDao.php
@@ -8,4 +8,12 @@ class ReviewsDao extends BaseDao
     {
         parent::__construct('reviews');
     }
+
+    public function getByField($field, $value)
+    {
+        $stmt = $this->connection->prepare("SELECT * FROM {$this->table} WHERE {$field} = :value");
+        $stmt->bindParam(':value', $value);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
 }
