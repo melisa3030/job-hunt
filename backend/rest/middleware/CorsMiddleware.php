ff776bee410c94bc4a896d57a4a267d50dc719d4
melisa3030 <Melisa.ahmetovic@stu.ibu.edu.ba>
2025-05-19 17:18:26 +0200

    update dev server port to 5173 in frontend and backend configurations

--- backend/rest/middleware/CorsMiddleware.php
+++ backend/rest/middleware/CorsMiddleware.php
@@ -2,7 +2,7 @@
 
 class CorsMiddleware {
     public static function handle() {
-        header('Access-Control-Allow-Origin: http://127.0.0.1:8080');
+        header('Access-Control-Allow-Origin: http://127.0.0.1:5173');
         header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
         header('Access-Control-Allow-Headers: Content-Type, Authorization');
         header('Access-Control-Allow-Credentials: true');
