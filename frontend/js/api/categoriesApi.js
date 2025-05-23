a9312f26e42e5da5e4bfbb4a6e2942d89a5355f4
melisa3030 <Melisa.ahmetovic@stu.ibu.edu.ba>
2025-05-23 23:34:36 +0200

    add categories API for fetching job categories

---
+++ frontend/js/api/categoriesApi.js
@@ -0,0 +1,27 @@
+import { AuthApi } from './authApi.js';
+import { BASE_URL } from '../constants/constants.js';
+
+export const CategoriesApi = {
+  async getAllCategories() {
+    try {
+      const response = await fetch(`${BASE_URL}/job_categories`, {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${AuthApi.getToken()}`,
+          'Content-Type': 'application/json',
+        },
+        credentials: 'include',
+      });
+
+      if (!response.ok) {
+        const errorText = await response.text();
+        console.error('Server response:', response.status, errorText);
+        throw new Error('Failed to get categories');
+      }
+      return response.json();
+    } catch (error) {
+      console.error('Error fetching categories:', error);
+      throw error;
+    }
+  },
+};
