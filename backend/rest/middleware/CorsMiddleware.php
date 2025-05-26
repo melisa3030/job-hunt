a6f31f159e705285e860f40db637ab15e561f02a
melisa3030 <Melisa.ahmetovic@stu.ibu.edu.ba>
2025-05-26 16:48:55 +0200

    update CORS middleware to allow all origins until production

--- backend/rest/middleware/CorsMiddleware.php
+++ backend/rest/middleware/CorsMiddleware.php
@@ -1,13 +1,15 @@
 <?php
 
-class CorsMiddleware {
-    public static function handle() {
-        header('Access-Control-Allow-Origin: http://127.0.0.1:5173');
+class CorsMiddleware
+{
+    public static function handle()
+    {
+        header('Access-Control-Allow-Origin: *');
         header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
         header('Access-Control-Allow-Headers: Content-Type, Authorization');
-        header('Access-Control-Allow-Credentials: true');
+        // Credentials must be false when using wildcard origin
+        header('Access-Control-Allow-Credentials: false');
 
-        // Handle preflight
         if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
             header('HTTP/1.1 200 OK');
             exit();
