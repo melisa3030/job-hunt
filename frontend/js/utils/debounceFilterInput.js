812a8b01c69c00f127f921c03c6ba31461151820
melisa3030 <Melisa.ahmetovic@stu.ibu.edu.ba>
2025-05-30 16:10:30 +0200

    refactor debounceFilterInput function to improve variable naming and consistency

--- frontend/js/utils/debounceFilterInput.js
+++ frontend/js/utils/debounceFilterInput.js
@@ -1,8 +1,8 @@
 // Utility function to debounce filter inputs (prevents too many updates while typing)
-export function debounceFilterInput(func, wait) {
-  let timeout;
+export function debounceFilterInput(func, delay) {
+  let timeoutId;
   return function (...args) {
-    clearTimeout(timeout);
-    timeout = setTimeout(() => func.apply(this, args), wait);
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => func.apply(this, args), delay);
   };
 }
